c7eacdb3174ead3699514804bd6ebbcde8781058
Mason Buchanan <masonwb@gmail.com>
2023-03-28 03:10:26 +0000

    prototype

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxCsvParserModule } from 'ngx-csv-parser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+import { ProgressComponent } from './progress/progress.component';
+import { DndDirective } from './dnd.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgressComponent,
+    DndDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxCsvParserModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
